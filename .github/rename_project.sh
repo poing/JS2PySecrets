5f83797bfae587f8a97c6b13a4ec09983c9653c2
poing <poing@users.noreply.github.com>
2024-01-10 07:18:37 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="poing"
+original_name="js2pysecrets"
+original_urlname="JS2PySecrets"
+original_description="Awesome js2pysecrets created by poing"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv js2pysecrets $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- js2pysecrets/cli.py
+++ js2pysecrets/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for js2pysecrets project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m js2pysecrets` and `$ js2pysecrets `.
 
     This is your program's entry point.
 
