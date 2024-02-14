fc359af270d08533bac9393b2717e9bbcfefd2bd
poing <brian.lavallee@invite-comm.jp>
2024-02-14 16:45:10 +0900

    Popen attempts, functional but NOT usable

--- gems/wrapperV7.js
+++ gems/wrapperV7.js
@@ -1,21 +1,21 @@
 // wrapperV7.js
 // Require the secrets package and assign it to a variable
 const secrets = require('../secrets.js/secrets.js');
-
-// Function to execute the commands sent from Python
-function executeCommands(input_data) {
-	
-
-    // Split the input_data into individual commands
-    const commands = input_data.split(';');
-    
-    // Execute each command
-    commands.forEach(command => {
-        eval(command); // Using eval to execute the command strings
-    });
-}
-
-console.log("Commands received from Python:", process.argv[2]));
-
-// Execute the commands sent from Python
-executeCommands(process.argv[2]);
+// 
+// // Function to execute the commands sent from Python
+// function executeCommands(input_data) {
+// 	
+// 
+//     // Split the input_data into individual commands
+//     const commands = input_data.split(';');
+//     
+//     // Execute each command
+//     commands.forEach(command => {
+//         eval(command); // Using eval to execute the command strings
+//     });
+// }
+// 
+// console.log("Commands received from Python:", process.argv[2]));
+// 
+// // Execute the commands sent from Python
+// executeCommands(process.argv[2]);
