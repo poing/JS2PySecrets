e82ec47e02b60242f125f60cbecbc57051774291
poing <brian.lavallee@invite-comm.jp>
2024-03-12 10:32:50 +0900

    Adjust test_with_UTF8 for test_win failure

--- tests/test_SecretsSpec.py
+++ tests/test_SecretsSpec.py
@@ -19,12 +19,9 @@ def test_with_UTF8():
     key = "¥ · £ · € · $ · ¢ · ₡ · ₢ · ₣ · ₤ · ₥ · ₦ · ₧ · ₨ · ₩ · ₪ · ₫ · ₭ · ₮ · ₯ · ₹"
     expected = "\xa5 \xb7 \xa3 \xb7 \u20ac \xb7 $ \xb7 \xa2 \xb7 \u20a1 \xb7 \u20a2 \xb7 \u20a3 \xb7 \u20a4 \xb7 \u20a5 \xb7 \u20a6 \xb7 \u20a7 \xb7 \u20a8 \xb7 \u20a9 \xb7 \u20aa \xb7 \u20ab \xb7 \u20ad \xb7 \u20ae \xb7 \u20af \xb7 \u20b9"
 
-    assert (
-        secrets.hex2str(
-            secrets.combine(secrets.share(secrets.str2hex(key), 3, 2))
-        )
-        == expected
-    )
+    assert secrets.hex2str(
+        secrets.combine(secrets.share(secrets.str2hex(key), 3, 2))
+    ).encode("utf-8", "strict") == key.encode("utf-8", "strict")
 
 
 # @pytest.mark.filterwarnings("ignore:secrets.share")
