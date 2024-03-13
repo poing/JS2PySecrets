af174923331a0b930475535a55f469051e44c44e
poing <brian.lavallee@invite-comm.jp>
2024-03-13 11:02:57 +0900

    Testing default setting as a class

---
+++ gems/defaults.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim: set et sw=4 fenc=utf-8:
+#
+# defaults.py
+
+import json
+import js2pysecrets as secrets
+from js2pysecrets.settings import Settings
+
+
+# Accessing Defaults
+settings = Settings()
+defaults = settings.get_defaults()
+print(defaults.bits)  # Output: 8
+
+# Updating Defaults
+settings.update_defaults(bits=16)
+defaults = settings.get_defaults()
+print(defaults.bits)  # Output: 16
+
+# Reverting to Default Values
+settings.update_defaults()
+defaults = settings.get_defaults()
+print(defaults.bits)  # Output: 8 (reverted to the default value)
+
+# Updating Defaults
+settings.update_defaults(bits=16)
+defaults = settings.get_defaults()
+print(defaults.bits)  # Output: 16
+
+# Reverting to Default Values
+settings.reset_defaults()
+defaults = settings.get_defaults()
+print(defaults.bits)  # Output: 8 (reverted to the default value)
+
+print(defaults.primitive_polynomials[12])
+
+print(defaults)
+
+# from js2pysecrets import wrapper
+# 
+# 
+# setup = []
+# main = {'function': 'getConfig', 'args': []}
+# tasks = {'tasks': [{'setup': setup, 'start': main}]}
+# json_data = json.dumps(tasks, indent=None)
+# print(wrapper(json_data))
+
+# print(js2pysecrets.getConfig())
+# print(js2pysecrets._reset())
+# print(js2pysecrets.getConfig())
+
