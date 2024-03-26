bd04d0508c702b0f40ce1bab1dddffc907e5b038
poing <brian.lavallee@invite-comm.jp>
2024-03-26 16:55:32 +0900

    Cleanup

--- js2pysecrets/base.py
+++ js2pysecrets/base.py
@@ -1,9 +1,3 @@
-# import json
-
-# from .decorators import JsFunction, jsNeedless
-
-# from .wrapper import wrapper  # Import your wrapper function
-
 import math
 import re
 
@@ -13,8 +7,6 @@ NAME = "js2pysecrets"
 
 
 settings = Settings()
-# settings.update_defaults(rng=99)
-# config = settings.get_config()
 
 
 def reset():
@@ -69,97 +61,6 @@ def hex2bin(hex_str):
         bin_str = bin(num)[2:].zfill(4) + bin_str
 
     return bin_str
-
-
-# def hex2bin(hex_string: str) -> str:
-#     binary_string = bin(int(hex_string, 16))[2:]
-#
-#     # Need to respect any leading zeros in the binary string when converting
-#     #  to hex.  Because 0b0011 = 0x3, while 0x03 is desired
-#     binary_string = binary_string.zfill((len(binary_string) // 8))
-#
-#     #binary_string = bin(int(hex_string, 16))[2:]
-#
-#     # Need to respect any leading zeros in the binary string when converting
-#     #  to hex.  Because 0b0011 = 0x3, while 0x03 is desired
-#     #binary_string = binary_string.zfill(int(hex_string, 2))
-#
-#     # return bin(int(hex_string, 16))[2:]
-#     return binary_string
-
-
-"""
-ABOVE THIS LINE ARE INTERNAL FUNCTIONS
-"""
-
-
-def init(bits=None, rngType=None):
-    exps = []
-    logs = [None]
-    x = 1
-    primitive = None
-
-    # reset all config back to initial state
-    reset()
-
-    if bits and (
-        not isinstance(bits, int)
-        or bits < settings.min_bits
-        or bits > settings.max_bits
-    ):
-        raise ValueError(
-            f"Number of bits must be an integer between {settings.min_bits} "
-            f"and {settings.max_bits}, inclusive."
-        )
-
-    if rngType and not isinstance(rngType, str) or callable(rngType):
-        raise ValueError(f"Invalid RNG type argument : '{rngType}'")
-
-    settings.update_defaults(radix=settings.radix)
-    settings.update_defaults(bits=bits or settings.bits)
-    settings.update_defaults(size=(2**settings.bits))
-    settings.update_defaults(maxShares=settings.size - 1)
-
-    # Construct the exp and log tables for multiplication.
-    primitive = settings.primitive_polynomials[settings.bits]
-
-    temp_logs = {}  # Temporary Dict to hold indexed logs
-    for i in range(settings.size):
-        # this works with loop below
-        exps.insert(i, x)
-        temp_logs[x] = i
-
-        x = x << 1  # Left shift assignment
-        if x >= settings.size:
-            x = x ^ primitive  # Bitwise XOR assignment
-            x = x & settings.maxShares  # Bitwise AND assignment
-
-    # Fill the logs List in the proper order
-    for i in range(1, settings.size):
-        logs.append(temp_logs[i])
-
-    settings.update_defaults(logs=logs)
-    settings.update_defaults(exps=exps)
-
-    if rngType:
-        settings.update_defaults(rng=rngType)
-
-    if not isSetRNG():
-        setRNG()  # pragma: no cover unlikely-and-redundant
-
-    if (
-        not isSetRNG()
-        or not settings.bits
-        or not settings.size
-        or not settings.maxShares
-        or not settings.logs
-        or not settings.exps
-        or len(settings.logs) != settings.size
-        or len(settings.exps) != settings.size
-    ):
-        raise ValueError(
-            "Initialization failed."
-        )  # pragma: no cover hard-to-fail
 
 
 """
@@ -236,6 +137,7 @@ def splitNumStringToIntArray(string, pad_length=None):
     return parts
 
 
+# Horner's method for polynomial evaluation.
 def horner(x, coeffs):
 
     logx = settings.logs[x]
@@ -255,6 +157,7 @@ def horner(x, coeffs):
     return fx
 
 
+# Lagrange interpolation formula for polynomial interpolation
 def lagrange(at, x, y):
     result = 0
     length = len(x)
@@ -358,99 +261,78 @@ def constructPublicShareString(bits, share_id, data):
     return new_share_string
 
 
-#
-# def base36decode(number):  # pragma: no cover
-#     return int(number, 36)
+"""
+ABOVE THIS LINE ARE INTERNAL FUNCTIONS
+"""
 
 
-# lambda bits: bin(random.getrandbits(bits))[2:].zfill(bits)
-# lambda bits: bin(secrets.randbits(bits))[2:].zfill(bits)
-# lambda bits: (bin(123456789)[2:].zfill(32) * -(-bits // 32))[-bits:]
-def setRNG(new_rng=None):
-    defaults = settings.get_defaults()
-    new_rng = new_rng or defaults.rng
-    settings.update_defaults(rng=new_rng)
-    return True
+def init(bits=None, rngType=None):
+    exps = []
+    logs = [None]
+    x = 1
+    primitive = None
 
+    # reset all config back to initial state
+    reset()
 
-def str2hex(string, bytes_per_char=None):
-    if not isinstance(string, str):
-        raise ValueError("Input must be a character string.")
-
-    defaults = settings.get_defaults()
-
-    # defaults = {"bytes_per_char": 2, "max_bytes_per_char": 4}
-    # Assuming these defaults
-    if bytes_per_char is None:
-        bytes_per_char = defaults.bytes_per_char
-
-    if (
-        not isinstance(bytes_per_char, int)
-        or bytes_per_char < 1
-        or bytes_per_char > defaults.max_bytes_per_char
+    if bits and (
+        not isinstance(bits, int)
+        or bits < settings.min_bits
+        or bits > settings.max_bits
     ):
         raise ValueError(
-            f"Bytes per character must be an integer between 1 and "
-            f"{defaults.max_bytes_per_char}, inclusive."
+            f"Number of bits must be an integer between {settings.min_bits} "
+            f"and {settings.max_bits}, inclusive."
         )
 
-    hex_chars = 2 * bytes_per_char
-    max_val = 16**hex_chars - 1
+    if rngType and not isinstance(rngType, str) or callable(rngType):
+        raise ValueError(f"Invalid RNG type argument : '{rngType}'")
 
-    out = ""
-    for char in string:
-        num = ord(char)
+    settings.update_defaults(radix=settings.radix)
+    settings.update_defaults(bits=bits or settings.bits)
+    settings.update_defaults(size=(2**settings.bits))
+    settings.update_defaults(maxShares=settings.size - 1)
 
-        if num > max_val:
-            needed_bytes = math.ceil(
-                math.log(num + 1) / math.log(256)
-            )  # pragma: no cover  Too difficult to test
-            raise ValueError(
-                f"Invalid character code ({num}). Maximum allowable is "
-                f"256^bytes-1 ({max_val}). To convert this character, use "
-                f"at least {needed_bytes} bytes."
-            )  # pragma: no cover  Too difficult to test
+    # Construct the exp and log tables for multiplication.
+    primitive = settings.primitive_polynomials[settings.bits]
 
-        out = format(num, f"0{hex_chars}x") + out
+    temp_logs = {}  # Temporary Dict to hold indexed logs
+    for i in range(settings.size):
+        # this works with loop below
+        exps.insert(i, x)
+        temp_logs[x] = i
 
-    return out
+        x = x << 1  # Left shift assignment
+        if x >= settings.size:
+            x = x ^ primitive  # Bitwise XOR assignment
+            x = x & settings.maxShares  # Bitwise AND assignment
 
+    # Fill the logs List in the proper order
+    for i in range(1, settings.size):
+        logs.append(temp_logs[i])
 
-def hex2str(hex_string, bytes_per_char=None):
-    if not isinstance(hex_string, str):
-        raise ValueError("Input must be a hexadecimal string.")
+    settings.update_defaults(logs=logs)
+    settings.update_defaults(exps=exps)
 
-    defaults = settings.get_defaults()
+    if rngType:
+        settings.update_defaults(rng=rngType)
 
-    # defaults = {"bytes_per_char": 2, "max_bytes_per_char": 4}
-    # Assuming these defaults
-
-    bytes_per_char = bytes_per_char or defaults.bytes_per_char
+    if not isSetRNG():
+        setRNG()  # pragma: no cover unlikely-and-redundant
 
     if (
-        not isinstance(bytes_per_char, int)
-        or bytes_per_char % 1 != 0
-        or bytes_per_char < 1
-        or bytes_per_char > defaults.max_bytes_per_char
+        not isSetRNG()
+        or not settings.bits
+        or not settings.size
+        or not settings.maxShares
+        or not settings.logs
+        or not settings.exps
+        or len(settings.logs) != settings.size
+        or len(settings.exps) != settings.size
     ):
         raise ValueError(
-            f"Bytes per character must be an integer between 1 and "
-            f"{defaults.max_bytes_per_char}, inclusive."
-        )
-
-    hex_chars = 2 * bytes_per_char
-
-    # Pad left if necessary
-    hex_string = hex_string.zfill(
-        len(hex_string) + (hex_chars - len(hex_string) % hex_chars) % hex_chars
-    )
-
-    out = ""
-    for i in range(0, len(hex_string), hex_chars):
-        char_code = int(hex_string[i : i + hex_chars], 16)
-        out = chr(char_code) + out
-
-    return out
+            "Initialization failed."
+        )  # pragma: no cover hard-to-fail
 
 
 def combine(shares, at=0):
@@ -552,6 +434,95 @@ def extractShareComponents(share):
     raise ValueError("Invalid share data provided.")
 
 
+# lambda bits: bin(random.getrandbits(bits))[2:].zfill(bits)
+# lambda bits: bin(secrets.randbits(bits))[2:].zfill(bits)
+# lambda bits: (bin(123456789)[2:].zfill(32) * -(-bits // 32))[-bits:]
+def setRNG(new_rng=None):
+    defaults = settings.get_defaults()
+    new_rng = new_rng or defaults.rng
+    settings.update_defaults(rng=new_rng)
+    return True
+
+
+def str2hex(string, bytes_per_char=None):
+    if not isinstance(string, str):
+        raise ValueError("Input must be a character string.")
+
+    defaults = settings.get_defaults()
+
+    # defaults = {"bytes_per_char": 2, "max_bytes_per_char": 4}
+    # Assuming these defaults
+    if bytes_per_char is None:
+        bytes_per_char = defaults.bytes_per_char
+
+    if (
+        not isinstance(bytes_per_char, int)
+        or bytes_per_char < 1
+        or bytes_per_char > defaults.max_bytes_per_char
+    ):
+        raise ValueError(
+            f"Bytes per character must be an integer between 1 and "
+            f"{defaults.max_bytes_per_char}, inclusive."
+        )
+
+    hex_chars = 2 * bytes_per_char
+    max_val = 16**hex_chars - 1
+
+    out = ""
+    for char in string:
+        num = ord(char)
+
+        if num > max_val:
+            needed_bytes = math.ceil(
+                math.log(num + 1) / math.log(256)
+            )  # pragma: no cover  Too difficult to test
+            raise ValueError(
+                f"Invalid character code ({num}). Maximum allowable is "
+                f"256^bytes-1 ({max_val}). To convert this character, use "
+                f"at least {needed_bytes} bytes."
+            )  # pragma: no cover  Too difficult to test
+
+        out = format(num, f"0{hex_chars}x") + out
+
+    return out
+
+
+def hex2str(hex_string, bytes_per_char=None):
+    if not isinstance(hex_string, str):
+        raise ValueError("Input must be a hexadecimal string.")
+
+    defaults = settings.get_defaults()
+
+    # defaults = {"bytes_per_char": 2, "max_bytes_per_char": 4}
+    # Assuming these defaults
+    bytes_per_char = bytes_per_char or defaults.bytes_per_char
+
+    if (
+        not isinstance(bytes_per_char, int)
+        or bytes_per_char % 1 != 0
+        or bytes_per_char < 1
+        or bytes_per_char > defaults.max_bytes_per_char
+    ):
+        raise ValueError(
+            f"Bytes per character must be an integer between 1 and "
+            f"{defaults.max_bytes_per_char}, inclusive."
+        )
+
+    hex_chars = 2 * bytes_per_char
+
+    # Pad left if necessary
+    hex_string = hex_string.zfill(
+        len(hex_string) + (hex_chars - len(hex_string) % hex_chars) % hex_chars
+    )
+
+    out = ""
+    for i in range(0, len(hex_string), hex_chars):
+        char_code = int(hex_string[i : i + hex_chars], 16)
+        out = chr(char_code) + out
+
+    return out
+
+
 def random(bits):
     if not isinstance(bits, int) or bits < 2 or bits > 65536:
         raise ValueError(
@@ -564,6 +535,7 @@ def random(bits):
 
 
 def share(secret, num_shares, threshold, pad_length=None):
+
     # Security: pad in multiples of 128 bits by default
     pad_length = pad_length or 128
 
@@ -640,33 +612,3 @@ def newShare(id, shares):
     raise ValueError(
         "Invalid 'id' or 'shares' Array argument to newShare()."
     )  # pragma: no cover
-
-
-# # Core Functions from secrets.js
-# init = jsFunction('init')
-# combine = jsFunction('combine')
-# getConfig = jsFunction('getConfig')
-# extractShareComponents = jsFunction('extractShareComponents')
-# setRNG = jsFunction('setRNG')
-# str2hex = jsFunction('str2hex')
-# hex2str = jsFunction('hex2str')
-# random = jsFunction('random')
-# share = jsFunction('share')
-# newShare = jsFunction('newShare')
-#
-#         /* test-code */
-#         // export private functions so they can be unit tested directly.
-#         _reset: reset,
-#         _padLeft: padLeft,
-#         _hex2bin: hex2bin,
-#         _bin2hex: bin2hex,
-#         _hasCryptoGetRandomValues: hasCryptoGetRandomValues,
-#         _hasCryptoRandomBytes: hasCryptoRandomBytes,
-#         _getRNG: getRNG,
-#         _isSetRNG: isSetRNG,
-#         _splitNumStringToIntArray: splitNumStringToIntArray,
-#         _horner: horner,
-#         _lagrange: lagrange,
-#         _getShares: getShares,
-#         _constructPublicShareString: constructPublicShareString
-#         /* end-test-code */
